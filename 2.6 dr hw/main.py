7e5257a8971133b7cba1f7156e7ca4a4cfa64fc6
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-04 01:20:10 +0300

    Add files via upload

---
+++ 2.6 dr hw/main.py
@@ -0,0 +1,17 @@
+set1  = {1,2,3,5,7}
+set2 = {2,8,90,7}
+result  = set1.union(set2)
+print(result)
+javob = set()
+for i in set1:
+        if i in set2:
+            javob.add(i)
+print(javob)
+inp = int(input("son kiriting")) 
+if inp in set1:
+    set1.remove(inp)
+    print(set1)
+import math
+print(set1)
+print(max(set1))
+print(min(set1))
